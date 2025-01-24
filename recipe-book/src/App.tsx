a7f8440ba026304ce4ea27984c77a0f454ede32f
Ana <anaadeladelrosario@gmail.com>
2025-01-24 10:01:36 +0100

    styling the burger so that when menu its open it shows a fork and a knife

--- recipe-book/src/App.tsx
+++ recipe-book/src/App.tsx
@@ -1,24 +1,22 @@
-
-import { AppHeader } from './components/AppHeader'
-import Hero from './components/Hero';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Form from './components/Form';
-import AussiePie from './components/AussiePie';
+import { AppHeader } from "./components/AppHeader";
+import Hero from "./components/Hero";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Form from "./components/Form";
+import Recipe from "./components/Recipe";
 
 function App() {
-
   return (
     <Router>
-    <div className="landing-page">
-      <AppHeader username="Ana" onLogout={() => {}} />
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/add-recipe" element={<Form />} />
-        <Route path="/aussie-pie" element={<AussiePie />} />
-      </Routes>
-    </div>
+      <div className="landing-page">
+        <AppHeader username="Ana" onLogout={() => {}} />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/add-recipe" element={<Form />} />
+          <Route path="/recipe" element={<Recipe />} />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
