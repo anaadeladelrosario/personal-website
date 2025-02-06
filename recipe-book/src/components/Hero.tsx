b1c5761b62ed089902fc19a7d4376178d52d5249
Ana <anaadeladelrosario@gmail.com>
2025-02-06 21:23:05 +0100

    removing add button

--- recipe-book/src/components/Hero.tsx
+++ recipe-book/src/components/Hero.tsx
@@ -1,8 +1,6 @@
-import { Button } from "./Button"
 import spices from '../assets/spices.jpg'
 import { Card } from "./Card"
 import './Hero.css'
-import { Link } from 'react-router-dom';
 import { RecipeIndex } from './RecipeIndex';
 
 function Hero() {
@@ -12,11 +10,8 @@ function Hero() {
         <img src={spices} alt="Hero Image" className="hero-image" />
           <h1 className="book-title">Your Recipe Book</h1>
       </div>
-       <div style={{  maxWidth: "800px", width: '90%', padding: 'var(--space-md)', borderRadius: 'var(--radius-md)', backgroundColor: 'var(--color-warning)', margin: 'var(--space-sm)', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '10px' }} className="button-section">
+       <div style={{  padding: 'var(--space-md)', borderRadius: 'var(--radius-md)', backgroundColor: 'var(--color-warning)', margin: 'var(--space-sm)', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '10px' }} className="button-section">
           <p>Check the menu to find a list of recipes or press add button to add a new recipe.</p>
-          <Link to="/add-recipe">
-            <Button size="small" primary label="Add" />
-          </Link>
         </div>
       <Card children={<RecipeIndex />} />
     </div>
