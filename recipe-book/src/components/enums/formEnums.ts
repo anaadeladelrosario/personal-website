65c45a39d966cbd80ecc8f3a2726bebd0adc3aed
Ana <anaadeladelrosario@gmail.com>
2025-02-17 13:01:17 +0100

    adding enums

---
+++ recipe-book/src/components/enums/formEnums.ts
@@ -0,0 +1,86 @@
+export enum Cuisine {
+  Australian = "Australian",
+  Brazilian = "Brazilian",
+  Chinese = "Chinese",
+  Cuban = "Cuban",
+  Italian = "Italian",
+  Japanese = "Japanese",
+  Spanish = "Spanish",
+  Swedish = "Swedish",
+}
+export const cuisines = [
+  Cuisine.Australian,
+  Cuisine.Brazilian,
+  Cuisine.Chinese,
+  Cuisine.Cuban,
+  Cuisine.Italian,
+  Cuisine.Japanese,
+  Cuisine.Spanish,
+  Cuisine.Swedish,
+];
+export enum Categories {
+  Appetizer = "Appetizer",
+  MainCourse = "MainCourse",
+  Dessert = "Dessert",
+  Snack = "Snack",
+}
+export const categories = [
+  Categories.Appetizer,
+  Categories.MainCourse,
+  Categories.Dessert,
+  Categories.Snack,
+];
+
+export enum Difficulty {
+  Easy = "Easy",
+  Medium = "Medium",
+  Hard = "Hard",
+}
+
+export const difficulties = [
+  Difficulty.Easy,
+  Difficulty.Medium,
+  Difficulty.Hard,
+];
+
+export enum CostRange {
+  Budget = "Budget",
+  Moderate = "Moderate",
+  Expensive = "Expensive",
+}
+export const costRanges = [
+  CostRange.Budget,
+  CostRange.Moderate,
+  CostRange.Expensive,
+];
+
+export enum Units {
+  Gram = "g",
+  Kilogram = "kg",
+  Milliliter = "ml",
+  Liter = "l",
+  Ounce = "oz",
+  Pound = "lb",
+  Pinch = "pinch",
+  Teaspoon = "tsp",
+  Tablespoon = "tbsp",
+  Cup = "cup",
+  Pint = "pint",
+  Quart = "quart",
+  Gallon = "gallon",
+}
+export const units = [
+  Units.Gram,
+  Units.Kilogram,
+  Units.Milliliter,
+  Units.Liter,
+  Units.Ounce,
+  Units.Pound,
+  Units.Pinch,
+  Units.Teaspoon,
+  Units.Tablespoon,
+  Units.Cup,
+  Units.Pint,
+  Units.Quart,
+  Units.Gallon,
+];
