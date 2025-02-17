3a8ef4bd7e41d62e3c610659e7ab05948bbeeae4
Ana <anaadeladelrosario@gmail.com>
2025-02-17 13:07:57 +0100

    cleaning card

--- recipe-book/src/components/Card.tsx
+++ recipe-book/src/components/Card.tsx
@@ -1,11 +1,22 @@
-import './Card.css';
+import "./Card.css";
+import "../styles/design-system.css";
 
-export const Card: React.FC<{children: React.ReactNode}> = ({children}) => {
+export const Card: React.FC<{ title?: string; children: React.ReactNode }> = ({
+  title,
+  children,
+}) => {
   return (
-    <div className="book-container">  
-        <div className="index-card">
-          {children}
-        </div>
+    <div className="container book-container">
+      <div
+        style={{
+          marginBottom: "var(--space-xs)",
+          textAlign: "center",
+          padding: "var(--space-xs)",
+        }}
+      >
+        {title && <p>{title}</p>}
+      </div>
+      {children}
     </div>
   );
-}; 
+};
--- recipe-book/src/components/InstructionCard.tsx
+++ recipe-book/src/components/InstructionCard.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import { InstructionsProps } from './Recipe';
+import React from "react";
+import { InstructionsProps } from "../components/interfaces/Recipe";
 
-const Instruction: React.FC<InstructionsProps> = ({ step, description, image }) => {
+const Instruction: React.FC<InstructionsProps> = ({
+  step,
+  description,
+  image,
+}) => {
   return (
-    <div style={{ display: 'flex', marginBottom: '10px' }}>
-      {image && <img src={image} alt={`Image for step ${step}`} style={{ width: '10%', fontWeight: 'bold' }} />}
-      <div style={{ width: '10%', fontWeight: 'bold' }}>{step}.</div>
-      <div style={{ width: '90%', textAlign: 'left' }}>{description}</div>
+    <div style={{ display: "flex", marginBottom: "10px" }}>
+      {image && (
+        <img
+          src={image}
+          alt={`Image for step ${step}`}
+          style={{ width: "10%", fontWeight: "bold" }}
+        />
+      )}
+      <div style={{ width: "10%", fontWeight: "bold" }}>{step}.</div>
+      <div style={{ width: "90%", textAlign: "left" }}>{description}</div>
     </div>
   );
 };
 
-const InstructionCard: React.FC<{instructions: InstructionsProps[]}> = ({instructions}) => {
-
+const InstructionCard: React.FC<{ instructions: InstructionsProps[] }> = ({
+  instructions,
+}) => {
   return (
-    <div>
-      <h2 style={{ marginBottom: '10px' }}>Directions</h2>
+    <div
+      style={{
+        flex: "1 1 300px",
+        backgroundColor: "white",
+        borderRadius: "var(--radius-lg)",
+        padding: "var(--space-xl)",
+        maxWidth: "800px",
+      }}
+    >
+      <h2>Directions</h2>
       {instructions.map((instruction) => (
-        <Instruction key={instruction.id} step={instruction.step} description={instruction.description} image={instruction.image} />
+        <Instruction
+          key={instruction.id}
+          step={instruction.step}
+          description={instruction.description}
+          image={instruction.image}
+        />
       ))}
     </div>
   );
