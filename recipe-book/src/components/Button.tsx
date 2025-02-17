3a1f251db9fdf1d04b54c84a9a1c48e315bffeb5
Ana <anaadeladelrosario@gmail.com>
2025-02-17 13:03:23 +0100

    refactoring button component

--- recipe-book/src/components/Button.tsx
+++ recipe-book/src/components/Button.tsx
@@ -1,33 +1,46 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export interface ButtonProps {
   primary?: boolean;
-  size?: 'small' | 'medium' | 'large';
-  label: string;
+  secondary?: boolean;
+  size?: "x-small" | "small" | "medium" | "large";
+  label?: string;
   onClick?: () => void;
-  type?: 'button' | 'submit';
+  type?: "button" | "submit" | "reset";
   disabled?: boolean;
   key?: number | string;
   style?: React.CSSProperties;
   children?: React.ReactNode;
+  className?: string;
 }
 
 export const Button = ({
   primary = false,
-  size = 'medium',
-  label,
-  type,
+  secondary = false,
+  size = "medium",
+  type = "button",
   disabled,
   key,
   children,
   style,
+  label,
+  className,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'button--primary' : 'button--secondary';
+  const mode = primary
+    ? "btn-primary "
+    : secondary
+    ? "btn-secondary"
+    : "btn-default";
   return (
-    <StyledButton label={"label"} 
+    <StyledButton
+      primary={primary}
+      secondary={secondary}
+      size={size}
       type={type}
-      className={['button', `button--${size}`, mode].join(' ')}
+      className={["button", `button--${size}`, mode, `${className}`]
+        .filter(Boolean)
+        .join(" ")}
       style={style}
       disabled={disabled}
       key={key}
@@ -40,9 +53,33 @@ export const Button = ({
 };
 
 const StyledButton = styled.button<ButtonProps>`
-  width: ${(props) => (props.size === 'small' ? 'fit-content' : props.size === 'large' ? '100%' : 'fit-content')};
-  font-size: ${(props) => (props.size === 'small' ? '12px' : props.size === 'large' ? '16px' : '14px')};
-  background-color: ${(props) => (props.primary ? 'var(--color-accent)' : 'var(--color-primary)')};
+  transition: filter 0.3s ease;
+  width: ${(props) =>
+    props.size === "x-small"
+      ? "fit-content"
+      : props.size === "small"
+      ? "fit-content"
+      : props.size === "large"
+      ? "100%"
+      : "fit-content"};
+  font-size: ${(props) =>
+    props.size === "x-small"
+      ? "var(--text-xs)"
+      : props.size === "small"
+      ? "var(--text-sm)"
+      : props.size === "large"
+      ? "var(--text-lg)"
+      : "var(--text-base)"};
+  background-color: ${(props) =>
+    props.primary
+      ? "var(--color-primary)"
+      : props.secondary
+      ? "var(--color-secondary)"
+      : ""};
   margin: var(--space-xs);
   cursor: pointer;
+  color: var(--color-text);
+  &:hover {
+    filter: brightness(1.15);
+  }
 `;
