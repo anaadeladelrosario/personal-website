dc6a29fa5c360900e83fded9461f99a96f02d014
Ana <anaadeladelrosario@gmail.com>
2025-02-18 21:46:01 +0100

    refactoring card

--- recipe-book/src/components/Card.tsx
+++ recipe-book/src/components/Card.tsx
@@ -1,22 +1,22 @@
 import "./Card.css";
 import "../styles/design-system.css";
+import styled from "styled-components";
 
 export const Card: React.FC<{ title?: string; children: React.ReactNode }> = ({
   title,
   children,
 }) => {
   return (
-    <div className="container book-container">
-      <div
-        style={{
-          marginBottom: "var(--space-xs)",
-          textAlign: "center",
-          padding: "var(--space-xs)",
-        }}
-      >
-        {title && <p>{title}</p>}
-      </div>
+    <BookContainer className="container">
+      {title && <p>{title}</p>}
       {children}
-    </div>
+    </BookContainer>
   );
 };
+
+export default Card;
+
+const BookContainer = styled.div`
+  text-align: center;
+  margin-bottom: var(--space-sm);
+`;
