8dbc68ff3727f9f4d79d68ed78b70035ab195541
Ana <anaadeladelrosario@gmail.com>
2025-02-18 20:37:22 +0100

    adding test logo for user

--- recipe-book/src/App.tsx
+++ recipe-book/src/App.tsx
@@ -1,16 +1,30 @@
-import { AppHeader } from "./components/AppHeader";
+import { AppHeader, User } from "./components/AppHeader";
 import Hero from "./components/Hero";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Form from "./components/Form";
 import Recipe from "./components/Recipe";
 import test from "./assets/test.jpg";
 import { RecipeIndex } from "./components/RecipeIndex";
+import userIcon from "./assets/user.svg";
+
+const user: User = {
+  name: "Ana",
+  avatar: userIcon,
+};
 
 function App() {
   return (
     <Router>
-      <div className="landing-page">
-        <AppHeader username="Ana" onLogout={() => {}} />
+      <div
+        style={{
+          backgroundSize: "30px 30px",
+          backgroundPosition: "0 0, 15px 15px",
+          backgroundImage:
+            "linear-gradient(90deg,rgba(214, 79, 41, 0.4) 50%,transparent 0),linear-gradient(0deg, rgba(214, 79, 41, 0.4) 50%, transparent 0)",
+        }}
+        className="landing-page"
+      >
+        <AppHeader user={user} onLogout={() => {}} />
         <Routes>
           <Route
             path="/"
@@ -27,6 +41,9 @@ function App() {
           <Route path="/edit/:recipeId" element={<Form />} /> {/* Edit route */}
           <Route path="/Recipe/:id" element={<Recipe />} />
         </Routes>
+        <div className="footer">
+          <p>Footer</p>
+        </div>
       </div>
     </Router>
   );
