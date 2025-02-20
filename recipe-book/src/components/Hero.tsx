b4c8455b820dbf73f671e6252382a2267c22a744
Ana <anaadeladelrosario@gmail.com>
2025-02-20 19:21:43 +0100

    hide lines in description in mobile devices

--- recipe-book/src/components/Hero.tsx
+++ recipe-book/src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import { Card } from "./Card";
 import "./Hero.css";
 import "../styles/design-system.css";
+import { useState } from "react";
 
 function Hero({
   title,
@@ -17,14 +18,32 @@ function Hero({
   cardTitle?: string;
   cardChildren?: React.ReactNode;
 }) {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleReadMore = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <div className="gingham-bg">
       <div className="hero-section">
-        {image && <img src={image} alt="Hero Image" className="hero-image" />}
+        {image && (
+          <img
+            src={image}
+            alt="Hero Image"
+            className="hero-image"
+            width="680"
+            height="260"
+          />
+        )}
         <div className="content">
           {children}
           <h1 className="book-title">{title}</h1>
-          {subtitle && <p className="book-subtitle">{subtitle}</p>}
+          {subtitle && (
+            <p className="book-subtitle" onClick={toggleReadMore}>
+              {showMore ? subtitle : `${subtitle?.slice(0, 50)}...`}
+            </p>
+          )}
         </div>
       </div>
       {cardTitle || cardChildren ? (
