f59a8d6cdc939815efe7ff741ee10fb4bc72df34
Ana <anaadeladelrosario@gmail.com>
2025-03-11 09:17:00 +0100

    removing old router

--- recipe-book/src/App.tsx
+++ recipe-book/src/App.tsx
@@ -10,54 +10,20 @@ import { Form } from "./components/Form";
 import { Recipe } from "./components/Recipe";
 import { NotFoundPage } from "./components/pages/NotFoundPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/add-recipe" element={<Form mode="create" />} />
-      <Route path="/edit/:recipeId" element={<Form mode="edit" />} />
-      <Route path="/Recipe/:id" element={<Recipe />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
-
 function App() {
-  return (
-    <RouterProvider router={router} />
-    // <Router>
-    //   <div
-    //     style={{
-    //       backgroundSize: "30px 30px",
-    //       backgroundPosition: "0 0, 15px 15px",
-    //       backgroundImage:
-    //         "linear-gradient(90deg,rgba(214, 79, 41, 0.4) 50%,transparent 0),linear-gradient(0deg, rgba(214, 79, 41, 0.4) 50%, transparent 0)",
-    //     }}
-    //     className="landing-page"
-    //   >
-    //     <AppHeader />
-    //     <Routes>
-    //       <Route
-    //         path="/"
-    //         element={
-    //           <Hero
-    //             title="Your Recipe Book"
-    //             subtitle="Click on a recipe to view its details"
-    //             image={test}
-    //             cardChildren={<RecipeIndex />}
-    //           />
-    //         }
-    //       />
-    //       <Route path="/add-recipe" element={<Form />} />
-    //       <Route path="/edit/:recipeId" element={<Form />} /> {/* Edit route */}
-    //       <Route path="/Recipe/:id" element={<Recipe />} />
-    //     </Routes>
-    //     <div className="footer">
-    //       <p>Footer</p>
-    //     </div>
-    //   </div>
-    // </Router>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/add-recipe" element={<Form mode="create" />} />
+        <Route path="/edit/:recipeId" element={<Form mode="edit" />} />
+        <Route path="/Recipe/:id" element={<Recipe />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
